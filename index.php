a9307873934a551587630c88d148d06cf74ccf5f
slavikusq75 <sl_msm@ukr.net>
2015-10-24 16:19:50 +0300

    Experement with var_dump

--- index.php
+++ index.php
@@ -5,50 +5,49 @@ require 'vendor/autoload.php';
 use HotelRooms\BungalowHotelRoom;
 use HotelRooms\MainBuildingHotelRoom;
 
-$room1=new BungalowHotelRoom(array
-(
+$room1=new BungalowHotelRoom(
+    [
     "parastatus"=>"1",
     "price"=>"200 USD per day",
     "number"=>"1",
-));
+    ]);
 
 $room1->dataOutput();
 
-$room2=new BungalowHotelRoom(array
-(
+$room2=new BungalowHotelRoom([
     "parastatus"=>"0",
     "price"=>"100 USD per day",
     "number"=>"2",
-));
+]);
 
 $room2->dataOutput();
 $room2->paranormalActivity(1);
 $room2->dataOutput();
 
 
-$room1408=new MainBuildingHotelRoom(array
-(
+$room1408=new MainBuildingHotelRoom(
+    [
     "parastatus"=>"1",
     "price"=>"200 USD per day",
     "number"=>"1408",
-));
+    ]);
 
 $room1408->dataOutput();
 $room1408->paranormalActivity(2);
 $room1408->dataOutput();
 
-$room1000=new MainBuildingHotelRoom(array
-(
+$room1000=new MainBuildingHotelRoom(
+    [
     "parastatus"=>"0",
     "price"=>"200 USD per day",
     "number"=>"1000",
-));
+    ]);
 
 $room1000->dataOutput();
 $room1000->paranormalActivity(0);
 $room1000->dataOutput();
 
-
+var_dump($room1);
 
 
 
